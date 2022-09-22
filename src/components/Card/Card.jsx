0d08584b86fd51c02954d1c88303b71a973bc2cb
Rafa Lozano <rafael@avena.io>
2022-09-21 22:41:43 -0500

    feat: add refresh advice

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,13 @@ import {getAdvice}from'../../api/getAdvice'
 import './card.css'
 const Card = () => {
     const [adviceData,setAdviceData] = useState(null);
-   
-    useEffect(() => {
+    const getAdviceData = () => {
         getAdvice()
         .then(data => setAdviceData(data.slip));
+    }
+   
+    useEffect(() => {
+        getAdviceData();
     }, [])
     
     return (
@@ -29,7 +32,7 @@ const Card = () => {
                     </g>
                 </svg>
             </div>
-            <div className="advice__diceContainer">
+            <div className="advice__diceContainer" onClick={getAdviceData}>
                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/>
                 </svg>
