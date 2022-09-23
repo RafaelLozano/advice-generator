bb93086914778d5dd2297d196d5ec4aa410f5b92
Rafa Lozano <rafael@avena.io>
2022-09-22 21:42:40 -0500

    fix: add hover state

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -14,17 +14,11 @@ const Card = () => {
     
     return (
         <article className="advice__card">
-            {adviceData ?
-            <>
-                <h1 data-testid="advice" >ADVICE #{adviceData?.id}</h1>
-                <h2>"{adviceData?.advice}"</h2>
-            </>:
-            <h2>Cargando...</h2>
-            }
-            <div className="divider">
-                
-            </div>
-            <div className="advice__diceContainer" 
+            <h1 data-testid="advice" >ADVICE #{adviceData?.id}</h1>
+            <h2>{adviceData?.advice ? `"${adviceData.advice}"`:'Cargando...'}</h2>
+            <div className="divider" />
+
+            <button className="advice__diceContainer" 
             onClick={ () => {
                 setAdviceData(null); 
                 getAdviceData();
@@ -33,7 +27,7 @@ const Card = () => {
                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/>
                 </svg>
-            </div>
+            </button>
         </article>
     )
 
